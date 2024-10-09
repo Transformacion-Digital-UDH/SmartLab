2b874cc172106a48fc58d05d08a73f594bd1bb61
wilmervasquez2 <wilmerluisvasquezlino@gmail.com>
2024-10-09 15:56:27 -0500

    Design Page Login

---
+++ resources/js/Components/Inputs/InputEmail.vue
@@ -0,0 +1,21 @@
+<script setup>
+  
+</script>
+<template>
+  <label for="" class="flex items-center p-3 gap-3 rounded-xl bg-blue-100 border-0">
+    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="#666"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class=""><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" /><path d="M3 7l9 6l9 -6" /></svg>
+    <input class="bg-transparent p-0 focus:ring-0 border-0 text-primary" type="email" placeholder="Correo electronico" >
+  </label>
+
+</template>
+<style scoped>
+  label:has(input:focus) {
+    outline: 1px solid #0866ff;
+    box-shadow: 0 0 6px #0866ff;
+    @apply bg-white;
+  }
+  label:has(input:focus) svg{
+    stroke: #0866ff;
+  }
+  
+</style>
--- resources/js/Pages/Login.vue
+++ resources/js/Pages/Login.vue
@@ -1,18 +1,31 @@
 <script setup>
+import InputEmail from '@/Components/Inputs/InputEmail.vue';
+import InputPassword from '@/Components/Inputs/InputPassword.vue';
+
   
 </script>
 <template>
   <div class="center grid place-items-center min-h-screen">
     <fieldset>
-      <legend>Iniciar Sesión</legend>
-      <form action="">
-        <input class="rounded-xl" type="email" placeholder="Correo eletrónico" >
+      <legend class="text-4xl text-center font-bold pb-4">Iniciar Sesión</legend>
+      <div class="flex items-center gap-3 pb-5">
+        <img src="" alt="" class="w-10 h-10 rounded-full bg-neutral-200">
+        <div class="flex flex-col">
+          <p class="text-blue-500 font-bold text-xs">Laboratorio</p>
+          <p class="font-bold">Transformacion digital</p>
+        </div>
+      </div>
+      <form action="" class="flex flex-col gap-3 w-[251px]">
+        <InputEmail/>
+        <InputPassword/>
+        <button class="bg-sky-700 font-bold text-white py-2 mt-4 rounded-xl">Ingresar</button>
+
+        <p class="pt-4">¿No tienes una cuenta? <a href="" class="text-blue-500 hover:underline underline-offset-2">Crear aquí</a></p>
       </form>
     </fieldset>
   </div>
 </template>
 <style scoped>
   .center {
-    place-items: center;
   }
 </style>
