16dbec92aead79a9ed8e419e5d96de972cacf665
wilmervasquez2 <wilmerluisvasquezlino@gmail.com>
2024-10-20 16:50:13 -0500

    definiendo la api de asistencia

---
+++ app/Http/Controllers/AsistenciaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Asistencia;
+use App\Models\MiembroProyecto;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class AsistenciaController extends Controller
+{
+    private function crearUsuarioInvitado($dni, $codigo) {
+        return User::create([
+            'id' => null,
+            'dni' => $dni,
+            'codigo' => $codigo,
+            'rol' => 'Invitado'
+        ]);
+    }
+    // GET
+    public function index(Request $request, $codigo) {
+        $user = null;
+        if (strlen($codigo) == 8) {
+            $user = User::where('dni', $codigo)->first();
+        } else {
+            $user = User::where('codigo', $codigo)->first();
+        }
+
+
+        if (!$user) {
+            $dni = strlen($codigo) == 8 ? $codigo : null;
+            $codigo = strlen($codigo) == 10 ? $codigo : null;
+            $user = $this->crearUsuarioInvitado($dni, $codigo);
+        } 
+        
+        $asistencias = Asistencia::where('usuario_id', $user->id)
+            ->whereDate('hora_entrada', Carbon::today())
+            ->get();
+        
+        $proyectos = MiembroProyecto::where('usuario_id', $user->id)->get();
+
+        return response()->json([
+            "id" => $user-> id,
+            "nombres"       => $user->nombres,
+            "apellidos"     => $user->apellidos,
+            "rol"           => $user->rol,
+            "tiene_entrada" => $user->rol,
+            "asistencias"   => $asistencias,
+            "proyectos"     => $proyectos,
+            // "\$user" => $user
+        ]);
+    }
+
+    // POST
+    public function registrarEntrada(Request $request){
+        $request -> validate([
+            'usuario_id' => 'required|integer'
+        ]);
+
+        $data = $request->all();
+        $usuario_id = $data['usuario_id'];
+        $proyecto_id = $data['proyecto_id'];
+
+        Asistencia::create([
+            'id'           => null,
+            'usuario_id'   => $usuario_id,
+            'hora_entrada' => now(),
+            'tarea'        => null,
+            'proyecto_id'  => $proyecto_id
+        ]);
+
+        return response('SE REGISTRO LA ENTRADA'.$data, 200);
+    }
+
+    // PUT
+    public function registrarSalida(Request $request){
+        return response('SE REGISTRO EXITOSAMENTE LA ASISTENCIA', 200);
+        $user_id = $request -> input('user_id');
+
+        $asistencia = Asistencia::where('user_id', $user_id)->latest('hora_entrada')->first();
+        $asistencia -> hora_salida = now();
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AsistenciaController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -17,12 +18,24 @@ Route::get('/login', function () {
     return Inertia::render('Login', []);
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified',
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return Inertia::render('Dashboard');
+//     })->name('dashboard');
+// });
+
+// Route::get('/api/asistencia/user', [AsistenciaController::class, 'informacionDelUsuario']);
+// Route::post('/api/asistencia/registrar_entrada', [AsistenciaController::class, 'registrarEntrada']);
+// Route::put('/api/asistencia/registrar_salida', [AsistenciaController::class, 'registrarSalida']);
+
+Route::prefix('api/asistencia') -> controller(AsistenciaController::class) -> group(function() {
+    Route::get('/user/{codigo}', 'index')
+        -> where('codigo', '^(\d{8}|\d{10})$');
+    Route::post('/registrar_entrada', 'registrarEntrada');
+    Route::put('/registrar_salida', 'registrarSalida');
 });
+
