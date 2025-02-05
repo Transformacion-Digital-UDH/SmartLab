a6f2267b1d603eadc08a8906366fd915c8ba3e9c
Wilmer Vasquez <2023210234@udh.edu.pe>
2025-02-05 00:28:14 -0500

    Agregue ReservaApiController y actualice CatalogoController para incluir el contacto administrativo; modifique los componentes Asistencia y Catalogo para mejorar la interfaz de usuario y la terminología

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AsistenciaController;
 use App\Http\Controllers\LaboratorioController;
+use App\Http\Controllers\ReservaApiController;
+use App\Models\Asistencia;
 
 // Las rutas que serán accedidas por aplicaciones externas se definen en api.php,
 // las que se usarán internamente en web.php
@@ -14,7 +16,6 @@ Route::prefix('/asistencia')->group(function () {
     Route::post('/registrar_entrada', [AsistenciaController::class, 'registrarEntrada']);
     Route::put('/registrar_salida', [AsistenciaController::class, 'registrarSalida']);
     Route::put('/eliminar/{id}', [AsistenciaController::class, 'eliminar']);
-
 });
 
 Route::prefix('/users')->group(function () {
@@ -24,4 +25,8 @@ Route::prefix('/users')->group(function () {
 Route::prefix('/laboratorios')->group(function () {
     Route::get('/', [LaboratorioController::class, 'info']);
     Route::post('/auth', [LaboratorioController::class, 'validar_lab']);
+});
+
+Route::prefix('/reservas')->group(function () {
+    Route::get('/{tipo}/{id}', [ReservaApiController::class, 'index']);
 });
